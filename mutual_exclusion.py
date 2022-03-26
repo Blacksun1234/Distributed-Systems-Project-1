ef8fcf58c7078ebd3dc88839c5aada836797c0fd
Manu <ecousin12@gmail.com>
2022-03-26 15:43:01 +0100

    Base

--- mutual_exclusion.py
+++ mutual_exclusion.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import sys
+
+def main(args):
+    if len(args) > 1:
+        try:
+            processes = args[1]
+            print(f"The Ricart-Agrawala algorithm will be executed with {processes} processes.")
+            var = True
+            while var:
+                command = input("Input a command line: ")
+        except :
+            print("Enter a valid number of processes.")
+
+if __name__ == "__main__":
+    main(sys.argv)
