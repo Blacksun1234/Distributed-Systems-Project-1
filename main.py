ed98e428a9d49e04d8c6ef1e7df5766407ee9d9d
blessing92 <percymunga02@gmail.com>
2022-04-02 15:07:35 +0300

    Implemented nodes with socket connections

--- main.py
+++ main.py
@@ -1,137 +1,72 @@
 import sys
-import datetime
-import _thread
 import time
-import random
-
-from state import State
+from node import Node
 
 
-# global variables
-processes = []
-system_start = datetime.datetime.now()
-resources = "MYRESOURCES"
-message = {}
+message = "critical"
 
 
-class CriticalSection():
-    def __init__(self) -> None:
-        self.time = 10
+# Using the callback we are able to see the events and messages of the Node
+def node_callback(event, main_node, connected_node, data):
+    global message
+    try:
+        if event == "node_message":
+            message = event + ":" + main_node.id + ":" + connected_node.id + ":" + str(data)
+
+    except Exception as e:
+        message = "exception: " + str(e) 
 
 
-class Process:
-    def __init__(self, id, logical_time):
-        self.id = id
-        self.logical_time = logical_time
-        self.elections = 0
-        self.time_start = time
-        self.set_time = time
-        self.t = 5
-        self.state = State.DO_NOT_WANT
-
-    # starts a thread that runs the process
-    def start(self):
-        _thread.start_new_thread(self.run, ())
+def start(args):
     
-    # Change the state of the process
-    def change_state(self):
-        pass
+    port = 8001
+    sockets = []
 
+    number_of_processes = int(args)
+    
+    for node_id in range(number_of_processes):
+        node = Node(port, "NODE_id_" + str(node_id+1), callback=node_callback)
+        port += 1
+        node.start()
+        sockets.append(node)
 
-    def run(self):
-        # with 5 second interval, update clock
-        while True:
-            time.sleep(5)
-            # self.update_clock()
-
-
-def tick(running, processes):
-    # program ticks evey second to reduce the time out by a second
-    # to update all the processors time out
-
+    running = True
+    
     while running:
-        time.sleep(1)
-        for p in processes:
-            # Update all processors time - out
-            p.t -= 1
+        command = input("Enter command (list) (Exit command to quit): ").lower()
+        
+
+        if command == 'list':
+            for node in sorted(sockets, key=lambda node: node.id):
+                print(str(node.id))
+        elif command == 'send':
+            for node in sorted(sockets, key=lambda node: node.id):
+                if node.id == 'NODE_id_1':
+                    node.connect_with_node("localhost", 8002)
+                    time.sleep(2)
+
+                    node.send_to_nodes("Hi from node 1!")
+                    time.sleep(1)
+                    node.node_message
+                    print(node.id, " bound to ",node.nodes_outbound)
+        elif command == 'exit':
+            for node in sockets:
+                node.stop()
+            print("Program Terminated Successfully! Please wait for all the nodes to stop...")
+            sys.exit()
+        else:
+            print("Invalid command! Please try again...")
 
             
 
-
-def list(processes):
-    # utility method to list proceeses
-    for p in processes:
-        print(f"P{str((p.id))}, {p.state.name}, {p.t}")
-
-
-def time_p(processes, t):
-    for p in processes:
-        random_t = random.randint(5, t)
-        p.t = random_t
-        
-
-
-def main(args):
-    # main program function
-    if len(args) > 1:
-        try:
-            # get the number of processes from terminal
-            number_of_processes = int(args[1])
-
-            for p in range(number_of_processes):     
-                processes.append(Process(p+1, 0))
-        except:
-            print("[ERROR] Failed to parse the input argument.")
-
-        print("Commands: list, time-cs (t), time-p (t), exit")
-    else:
-        print("Please run the program with the number of processes!")
-
-    # start threads of all processes
-    for p in processes:
-        p.start()
-
-    # start the main loop
-    running = True
-
-    # start a separate thread for system tick
-    # _thread.start_new_thread(tick, (running, processes))
-
-    while running:
-        inp = input().lower()
-        cmd = inp.split(" ")
-
-        command = cmd[0]
-
-        if len(cmd) > 3:
-            print("Too many arguments")
-
-        # handle exit
-        elif command == "exit":
-            running = False
-
-        # handle list
-        elif command == "list":
-            try:
-                list(processes)
-            except:
-                print("Error in list")
-
-        # handle kill
-        elif command == "time-p":
-            try:
-                print("Command line ", int(cmd[1]))
-                time_p(processes, int(cmd[1]))
-            except:
-                print("Error in time")
-
-
-        # handle unsupported command        
-        else:
-            print("Unsupported command:", inp)
-
-    print("Program exited")
-
+def get_usage():
+    print("Usage: python main.py <number of processes>")
+    sys.exit(1)
 
 if __name__ == "__main__":
-    main(sys.argv)
+    if len(sys.argv) != 2:
+        get_usage()
+    else:
+        start(sys.argv[1])        
+
+
