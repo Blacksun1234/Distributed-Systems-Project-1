e4c9f978d99da0973ddc381772a607337940ca90
blessing92 <percymunga02@gmail.com>
2022-04-04 16:21:52 +0300

    Update node to check incoming messages

--- main2.py
+++ main2.py
@@ -23,6 +23,7 @@ class MyTestNode (Node):
         self.timestamp = 0
         self.time_to_hold_cs = 10
         self.state = State.DO_NOT_WANT
+        self.local_count = 0
         self.queues = []
 
         super(MyTestNode, self).__init__(host, port, id)
@@ -47,6 +48,7 @@ class MyTestNode (Node):
 
     def node_message(self, node, data):
         global message
+        num_node = len(sockets) - 1
         self.ownMessage.append(self.id + " node_message from " + node.id + ": " + str(data) + " port: " + str(node.port))
 
         # Nodes_inbound: Nodes that are connected to us 
@@ -69,32 +71,33 @@ class MyTestNode (Node):
 
                 new_msg = "ok" + "," + self.id + "," + str(time.monotonic())
                 self.send_to_node(node, new_msg)
-
-                if node in self.nodes_inbound:
-                    # n.send(data)
-                    # print(self.id, " has ", node.id, "in is nodes_inbound")
-                    pass
-                
-                if node in self.nodes_outbound:
-                    # print(self.id, " has ", node.id, "in is nodes_outbound")
-                    pass
+                time.sleep(5)
             else:
                 self.queues.append(str(data))
+            
+            for msg in self.ownMessage:
+                response = msg.split(':')[1].split(",")[0]
+                if str(response.strip()) == "ok":
+                    self.local_count += 1
+
+            if num_node == self.local_count:
+                print(self.id, "------ Changing state ------ (2)")
+                self.state = State.HELD
+                time.sleep(self.time_to_hold_cs)
+                self.state = State.DO_NOT_WANT
+                # self.ownMessage = []
+                self.local_count = 0
+                
+
 
         # if the receiver not accessing the resource
         elif self.state.name == 'DO_NOT_WANT':
             # Reply to node by sending ok, because I do not want to access the critical section
                 new_msg = "ok" + "," + self.id + "," + str(time.monotonic())
                 self.send_to_node(node, new_msg)
+                time.sleep(5)
+        
 
-
-        # if len(self.nodes_inbound) != 0:
-        #     for n in self.nodes_inbound:
-        #         self.send_to_node(n, "ok")
-        #         time.sleep(2)
-        # if len(self.ownMessage) > 0:
-        #     self.send_to_nodes('ok')
-        #     time.sleep(2)
    
         print(self.ownMessage)
         
@@ -118,6 +121,7 @@ def tick(running, node):
         time.sleep(node.logical_time)
         node.state = State.WANTED
         my_id = node.id
+        my_node = node
         send_time = time.monotonic()
         new_msg = "resource" + "," + node.id + "," + str(send_time)
         
@@ -136,25 +140,36 @@ def tick(running, node):
 
                     # Wait for all the reply, then check the incoming messages
                     messages = node.ownMessage
-                    print(node.id, " From while: ", messages)
                     for msg in messages:
                         response = msg.split(':')[1].split(",")[0]
-                        print(node.id, " This is response ---> ", response)
-                        if str(response) == "ok":
+                        if str(response.strip()) == "ok":
                             count_response += 1
                     
                     print(node.id, " number of ok responses: ", count_response)
                     # Check if the number of responses is equal to the number of send messages
                     if num_node == count_response:
+                        print(my_id, "------ Changing state ------ (1)")
                         node.state = State.HELD
                         time.sleep(node.time_to_hold_cs)
                         node.state = State.DO_NOT_WANT
+                        # node.ownMessage = []
+                        node.local_count = 0
+                        count_response = 0
+                        # alread_sent = True
+                        node.timestamp = 0
 
+                        # Send ok message to those asked for the resource 
+                        for okmsg in messages:
+                            response = okmsg.split(':')[1].split(",")[0]
+                            node_id = okmsg.split(':')[0].split(" ")[3]
 
-
-
-
-
+                            if str(response.strip()) == "resource":
+                                for node in sockets:
+                                    if node.id == node_id:
+                                        new_msg = "ok" + "," + my_id + "," + str(time.monotonic())
+                                        my_node.send_to_node(node, new_msg)
+                                        time.sleep(5)
+                                
 def start(args):
 
     port = 10001
@@ -186,12 +201,12 @@ def start(args):
 
         if command == 'list':
             for node in sorted(sockets, key=lambda node: node.id):
-                print(str(node.id) + " " + str(node.state.name) + " MSG R: " + str(node.message_count_recv))
+                print(str(node.id) + " " + str(node.state.name) + " MSG R: " + str(node.message_count_recv) + " " + str(node.logical_time) + " " + str(node.local_count))
         elif command == "time-p":
-            time_p(sockets, int(cmd[1]))
+            time_p(int(cmd[1]))
         
         elif command == "time-cs":
-            time_cs(sockets, int(cmd[1]))
+            time_cs(int(cmd[1]))
 
         elif command == 'exit':
             for node in sockets:
@@ -202,14 +217,20 @@ def start(args):
             print("Invalid command! Please try again...")
 
 
-def time_p(sockets, p):
+def time_p(p):
+    if p <= 5:
+        print("Time-p should be greater or equal to 5")
+
     for node in sorted(sockets, key=lambda node: node.id):
         random_p = random.randint(5, p)
         node.logical_time = random_p
 
-def time_cs(sockets, cs):
+def time_cs(cs):
+    if cs <= 10:
+        print("Time-cs should be greater or equal to 10")
+
     for node in sorted(sockets, key=lambda node: node.id):
-        node.cs = random.randint(10, cs)
+        node.time_to_hold_cs = random.randint(10, cs)
 
 
 def get_usage():
